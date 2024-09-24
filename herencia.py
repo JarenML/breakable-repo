ee033489121471c9b3fec18c09aecf9360eb0bc2
Jaren Ramos <jarenramos234@gmail.com>
2024-09-24 17:38:27 -0500

    Creando clase hija (Perro)

--- herencia.py
+++ herencia.py
@@ -8,3 +8,9 @@ animal = Animal("Rex", 5)
 animal2 = Animal("Panda", 2)
 
 animal3 = Animal("Michifus", 6)    
+
+
+class Perro(Animal):
+    def ladrar(self):
+        print("el perro esta ladrando")
+
