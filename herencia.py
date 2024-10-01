831ffe5b90ff193d4deca5022da8dfae854d799d
Jaren Ramos <jarenramos234@gmail.com>
2024-09-30 19:34:51 -0500

    Eliminando instancias

--- herencia.py
+++ herencia.py
@@ -19,18 +19,7 @@ class AveNoVoladora(Ave):
     pass
     
 
-animal = Animal("Rex", 5)
-
-animal2 = Animal("Panda", 2)
-
-animal3 = Animal("Michifus", 6)    
-
-
 class Perro(Animal):
     def ladrar(self):
         print("el perro esta ladrando")
-
-perro1 = Perro("Toby", 3)
-
-perro2 = Perro("Firulais", 1)
 
